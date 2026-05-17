82a7b4bff5377d54c49c97813fd0bd7556dcb64e
Vova-luk <volf.lukonin@bk.ru>
2026-05-17 20:43:03 +0300

    Add implementation of ParseStrategyTask function using stubs

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -1,11 +1,62 @@
 #include <vector>
 #include "./tic_tac_toe.hpp"
 
+const int NO_POSITION = -1;
+const int TOTAL_INPUT_LINES = 5;
+
 bool ParseStrategyTask(
     const std::vector<std::string>& lines,
     StrategyTask& task,
     Error& error
 ) {
+    // Проверить, что входной файл не пуст.
+    if (lines.empty()) {
+        error.type = EMPTY_INPUT_ERROR;
+        error.file_path = "";
+        error.row = NO_POSITION;
+        error.column = NO_POSITION;
+        return false;
+    }
+
+    // Проверить, что количество строк не меньше пяти.
+    if (lines.size() < TOTAL_INPUT_LINES) {
+        error.type = MISSING_LINES_ERROR;
+        error.file_path = "";
+        error.row = NO_POSITION;
+        error.column = NO_POSITION;
+        return false;
+    }
+
+    // Проверить, что количество строк не больше пяти.
+    if (lines.size() > TOTAL_INPUT_LINES) {
+        error.type = EXTRA_LINES_ERROR;
+        error.file_path = "";
+        error.row = NO_POSITION;
+        error.column = NO_POSITION;
+        return false;
+    }
+
+    //Распарсить поле.
+    if (!ParseBoardLines(lines, task.board, error)) {
+        return false;
+    }
+
+    //Распарсить строки игроков функцией.
+    if (!ParsePlayers(lines, task, error)) {
+        return false;
+    }
+
+    // Проверить корректность количества фигур и побед
+    if (!ValidateCountsAndWins(task.board, error)) {
+        return false;
+    }
+
+    // Проверить корректность следующего игрока
+    if (!ValidateNextPlayer(task.board, task.next_player, error)) {
+        return false;
+    }
+    
+    // Вернуть true, если все проверки пройдены успешно.
     return true;
 }
 
